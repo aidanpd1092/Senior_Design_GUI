692081247b3ca7461a25a36191338fbc22c432e9
kmb1324 <kmb1324@gmail.com>
2015-04-05 22:52:56 -0400

    added Throat Mic Code

---
+++ Throat_Microphone_Code/VoiceRecorderBest/VoiceRecorderBest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace VoiceRecorderBest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.recordButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.customWaveViewer1 = new VoiceRecorderBest.CustomWaveViewer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(106, 239);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(108, 33);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save Location";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // recordButton
+            // 
+            this.recordButton.Location = new System.Drawing.Point(41, 278);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(108, 37);
+            this.recordButton.TabIndex = 1;
+            this.recordButton.Text = "Record";
+            this.recordButton.UseVisualStyleBackColor = true;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(178, 278);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(112, 37);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop Recording";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // customWaveViewer1
+            // 
+            this.customWaveViewer1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.customWaveViewer1.Location = new System.Drawing.Point(21, 12);
+            this.customWaveViewer1.Name = "customWaveViewer1";
+            this.customWaveViewer1.PenColor = System.Drawing.Color.DodgerBlue;
+            this.customWaveViewer1.PenWidth = 1F;
+            this.customWaveViewer1.SamplesPerPixel = 128;
+            this.customWaveViewer1.Size = new System.Drawing.Size(404, 221);
+            this.customWaveViewer1.StartPosition = ((long)(0));
+            this.customWaveViewer1.TabIndex = 3;
+            this.customWaveViewer1.WaveStream = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Wave";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 339);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.customWaveViewer1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.recordButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.Text = "Audio Record";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button recordButton;
+        private System.Windows.Forms.Button stopButton;
+        private CustomWaveViewer customWaveViewer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Throat_Microphone_Code/VoiceRecorderBest/VoiceRecorderBest/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Mixer;
+using System.Globalization;
+using System.Threading;
+
+namespace VoiceRecorderBest
+{
+    public partial class Form1 : Form
+    {
+        public WaveIn waveIn = null;
+        public WaveFileWriter waveFile = null;
+        public string myPath = System.IO.Path.Combine("C://Users/Kevin/Documents/AudioTesting");
+        public string path_wav;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.recordButton.IsEnabled = true;
+            this.stopButton.Visible = false;
+        }
+
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+            record();
+            this.recordButton.Visible = false;
+            this.stopButton.Visible = true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            waveIn.StopRecording();
+            this.recordButton.Visible = true;
+            this.stopButton.Visible = false;
+            //Thread.Sleep(100);
+            
+            
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            myPath = dialog.SelectedPath;
+        }
+
+        public void record()
+        {
+            waveIn = new WaveIn();
+            //waveIn.DeviceNumber = deviceNumber;
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStopped);
+            string time = System.DateTime.Now.ToString("yyyy'-'M'-'d'-'hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+            string fname = String.Format("AudioTest_{0}.wav", time);
+            path_wav = System.IO.Path.Combine(myPath, fname);
+            waveFile = new WaveFileWriter(path_wav, waveIn.WaveFormat);
+
+            waveIn.StartRecording();
+
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        void waveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveIn != null)
+            {
+                waveIn.Dispose();
+                waveIn = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customWaveViewer1.WaveStream = new NAudio.Wave.WaveFileReader(path_wav);
+            customWaveViewer1.FitToScreen();
+        }
+    }
+}
